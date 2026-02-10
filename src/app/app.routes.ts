c8914409bf7b5a177774cec90d1a8e214e0c7c6d
houssam <lambarahoussam@gmail.com>
2026-02-10 23:30:09 +0100

    Configuration des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth/login',
+        loadComponent: () => import('./features/auth/login/login').then(m => m.LoginComponent)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then(m => m.HomeComponent)
+    },
+    {
+        path: 'auth/register',
+        loadComponent: () => import('./features/auth/register/register').then(m => m.RegisterComponent)
+    },
+    {
+        path: 'favorites',
+        loadComponent: () => import('./features/favorites/favorites').then(m => m.FavoritesComponent)
+    },
+    {
+        path: 'applications',
+        loadComponent: () => import('./features/applications/applications').then(m => m.ApplicationsComponent)
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('./features/profile/profile').then(m => m.ProfileComponent)
+    },
+    {
+        path: 'jobs/:id',
+        loadComponent: () => import('./features/jobs/job-details').then(m => m.JobDetailsComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    }
+];
