398219d2761b1f6567cdab636221a9d0bf9a69e4
houssam <lambarahoussam@gmail.com>
2026-02-11 12:51:50 +0100

    Ajout de la page profil

---
+++ src/app/features/profile/profile.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-profile',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './profile.html'
+})
+export class ProfileComponent { }
