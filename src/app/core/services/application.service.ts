2ac2cbdcc7b04999f55d881857192e2699448a19
houssam <lambarahoussam@gmail.com>
2026-02-15 23:36:31 +0000

    Mise à jour du Service de gestion des candidatures

---
+++ src/app/core/services/application.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../env/environment';
+import { Application, ApplicationStatus } from '../models/application.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApplicationService {
+    private http = inject(HttpClient);
+    private apiUrl = `${environment.jsonServerUrl}/applications`;
+
+    getApplications(userId: string | number): Observable<Application[]> {
+        return this.http.get<Application[]>(`${this.apiUrl}?userId=${userId}`);
+    }
+
+    addApplication(application: Application): Observable<Application> {
+        return this.http.post<Application>(this.apiUrl, application);
+    }
+
+    updateStatus(id: string | number, status: ApplicationStatus): Observable<Application> {
+        return this.http.patch<Application>(`${this.apiUrl}/${id}`, { status });
+    }
+
+
+
+    deleteApplication(id: string | number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+}
