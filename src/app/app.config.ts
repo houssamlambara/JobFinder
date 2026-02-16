23520b47b4e7fce393f4218e5f9bb25e25b3579c
houssam <lambarahoussam@gmail.com>
2026-02-16 09:50:35 +0000

    Configuration globale du Store NgRx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { favoritesReducer } from './core/store/favorites/favorite.reducer';
+import { FavoritesEffects } from './core/store/favorites/favorite.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes, withComponentInputBinding(), withViewTransitions())
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideHttpClient(withFetch(), withInterceptors([loadingInterceptor])),
+    provideAnimations(),
+    provideStore({
+      favorites: favoritesReducer
+    }),
+    provideEffects([FavoritesEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ]
 };
